8aab20e40c7b7354ab709fbadf20ce7ff014e94f
andanao <adriandanao@gmail.com>
2022-12-28 22:43:29 -0500

    typo

--- .config/zsh/aliases/ceres.zsh
+++ .config/zsh/aliases/ceres.zsh
@@ -1,6 +1,6 @@
 # Ceres specific aliases
 
 # ZMK Stuff
-alias zmk_mount="udiskctl mount -b /dev/sda"
+alias zmk_mount="udisksctl mount -b /dev/sda"
 alias zmk_zip="unzip ~/downloads/firmware.zip"
 alias zmk_clean="cd ~/downloads && rm firmware.zip ; rm *.uf2* "
