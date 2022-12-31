d000323085e726b79d1081ccb29bd593a4961cf0
andanao <adriandanao@gmail.com>
2022-12-30 23:46:08 -0500

    zmk l and r

--- .config/zsh/aliases/ceres.zsh
+++ .config/zsh/aliases/ceres.zsh
@@ -4,3 +4,5 @@
 alias zmk_mount="udisksctl mount -b /dev/sda"
 alias zmk_zip="unzip ~/downloads/firmware.zip && rm firmware.zip"
 alias zmk_clean="cd ~/downloads && rm *.uf2* "
+alias zmk_l="zmk_mount && mv corne_left-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO "
+alias zmk_r="zmk_mount && mv corne_right-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO && zmk_clean"
