c696741d7c80982e29316ef65320dc5ba67c3fdf
andanao <adriandanao@gmail.com>
2023-01-28 12:42:33 -0500

    zsh alias mount sd

--- .config/zsh/aliases/ceres.zsh
+++ .config/zsh/aliases/ceres.zsh
@@ -6,3 +6,6 @@ alias zmk_zip="unzip ~/downloads/firmware.zip && rm firmware.zip"
 alias zmk_clean="cd ~/downloads && rm *.uf2* "
 alias zmk_l="zmk_mount && mv corne_left-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO "
 alias zmk_r="zmk_mount && mv corne_right-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO && sleep 2 && zmk_clean"
+
+# General stuff
+alias mount_sd="udisksctl mount -b /dev/mmcblk0p1"
