3fab039be35e2f59d5d099b4330eb53f93bd5e96
andanao <adriandanao@gmail.com>
2023-03-13 08:27:24 -0400

    clean zmk aliases

--- .config/zsh/aliases/ceres.zsh
+++ .config/zsh/aliases/ceres.zsh
@@ -4,8 +4,9 @@
 alias zmk_mount="udisksctl mount -b /dev/sda"
 alias zmk_zip="unzip ~/downloads/firmware.zip && rm firmware.zip"
 alias zmk_clean="cd ~/downloads && rm *.uf2* "
-alias zmk_l="zmk_mount && mv corne_left-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO "
-alias zmk_r="zmk_mount && mv corne_right-nice_nano_v2-zmk.uf2 /run/media/adrian/NICENANO && sleep 2 && zmk_clean"
+alias zmk_l="zmk_mount && mv corne_left*.uf2 /run/media/adrian/NICENANO "
+alias zmk_r="zmk_mount && mv corne_right.uf2 /run/media/adrian/NICENANO && sleep 2 && zmk_clean"
+alias zmk_lazy="zmk_clean && zmk_zip && zmk_mount && sleep 1 && zmk l && zmk_clean"
 
 # General stuff
 alias mount_sd="udisksctl mount -b /dev/mmcblk0p1"
